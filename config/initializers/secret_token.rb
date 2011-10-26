e01d5242378e8a1758a534806d98ec48516d85f0
Ron 22118 <ron22118@gmail.com>
2011-10-26 11:46:40 -0500

    v0.0 Bare Rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bciab::Application.config.secret_token = '9c9a5cd92a60399f69f3166368a356d6967ad0b46c74b291c4df174153d0e0156ca72cab0c375506ec2653a45073f9be21bac345377b0947793c70274a60a4fa'
